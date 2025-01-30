51e4f5ea05c57937200a8b31cd46167e214e303c
Clark Productions <clarkproductions05@gmail.com>
2025-01-30 14:18:10 -0500

    Add files via upload

---
+++ fivem_loading/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'style.css',
+    'script.js',
+    'logo.png',
+    'background.jpg',
+    'music.mp3'
+}
+
+loadscreen_manual_shutdown 'yes'
